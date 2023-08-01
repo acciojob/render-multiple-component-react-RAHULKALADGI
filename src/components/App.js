8a584fb952c30e7b9a7798c3c8401f263ad7e7e7
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-01 20:41:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <h1 data-ns-test="project-name">Typing Test App</h1>
+    <p data-ns-test="project-description">Developed a dynamic Typing Test Application using ReactJS and Firebase, offering real-time typing tests and progress tracking for users to enhance their typing skills. Utilized Firestore to store and track users test results and progress, allowing for seamless access to performance history across devices.</p>
+    </div>
   )
 }
 
